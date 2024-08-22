e93db9fb31e79317c22d31a1783078c5f2e143ab
Phuong Nguyen <phuongsover2@gmail.com>
2024-08-23 00:56:35 +0700

    async/await

--- 02-JS-review/async.js
+++ 02-JS-review/async.js
@@ -1,5 +1,15 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
-.then(res => res.json())
-.then(data => console.log(data));
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
 
-console.log('phuong');
+// console.log("phuong");
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+  const data = await res.json();
+  console.log(data);
+}
+
+const todo = getTodos();
+console.log(todo);
+console.log("jonas");
