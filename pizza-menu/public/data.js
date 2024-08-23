751364cd2edc37ee27c5134b1e6470858583ec9a
Phuong Nguyen <phuongsover2@gmail.com>
2024-08-23 22:04:21 +0700

    creating and reusing a component

--- pizza-menu/public/data.js
+++ pizza-menu/public/data.js
@@ -1,6 +1,3 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-
 const pizzaData = [
   {
     name: "Focaccia",
@@ -45,54 +42,3 @@ const pizzaData = [
     soldOut: false,
   },
 ];
-
-function App() {
-  return (
-    <div>
-      <Header />
-      <Menu />
-      <Footer />
-    </div>
-  );
-}
-
-function Pizza() {
-  return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-  );
-}
-
-function Header() {
-  return <h1>Fast React Pizza Co.</h1>;
-}
-
-function Menu() {
-  return (
-    <div>
-      <h2>Our menu</h2>;
-      <Pizza />
-    </div>
-  );
-}
-
-function Footer() {
-  return (
-    <footer>{new Date().toLocaleTimeString()} We're curreny open!!! </footer>
-  );
-}
-
-// React v18
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// React before 18
-//ReactDOM.render(<App />);
