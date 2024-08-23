59e5471716ef380c37c455c34c8e09f3629e440e
Phuong Nguyen <phuongsover2@gmail.com>
2024-08-23 22:14:17 +0700

    js logic in components

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -80,6 +80,11 @@ function Menu() {
 }
 
 function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour > openHour && hour < closeHour;
+
   return (
     <footer>{new Date().toLocaleTimeString()} We're curreny open!!! </footer>
   );
